34c0b0a62e3ab0c673581922c820420d2f0ca44a
Anderson Diniz <113947327+AndersonDinizDev@users.noreply.github.com>
2024-05-08 21:51:00 +0000

    correção.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Routes from './routes';
-import GlobalStyle from './styles/globalStyle'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Routes from "./routes";
+import GlobalStyle from "./styles/globalStyle";
 
-import ParticlesComponent from './components/teste';
+import ParticlesComponent from "./components/particlecontainer";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <><Routes /><ParticlesComponent id="particles" /> <GlobalStyle /></>
+  <>
+    <Routes />
+    <ParticlesComponent id="particles" /> <GlobalStyle />
+  </>
 );
